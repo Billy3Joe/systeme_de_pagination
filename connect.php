0dc4665363f20f40f5672e1a3f670bdbbdef91b4
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2022-09-06 13:37:09 +0200

    système de pagination php

---
+++ connect.php
@@ -0,0 +1,21 @@
+
+<?php 
+    session_start();
+    
+    $dbhost = "localhost";
+    $dbname = "blog";
+    $dbuser = 'root';
+    $dbpswd = "";
+
+  try {
+      $db = new PDO("mysql:host=$dbhost;dbname=$dbname", 
+      $dbuser, $dbpswd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  }catch(PDOException $e){
+
+    echo "Une erreur est survenue lors de la connexion à la base de données : ". $e->getMessage();
+  }
+
+?>
